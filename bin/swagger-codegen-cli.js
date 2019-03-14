4bfd5450f3f5389ddcf453b83fc36504843950ca
sius <sius@gmx.net>
2019-03-14 22:46:48 +0100

    fix bin script

--- bin/swagger-codegen-cli.js
+++ bin/swagger-codegen-cli.js
@@ -1,5 +1,5 @@
 #!/usr/bin/env node
 require('child_process')
   .spawn('java', [
-    '-jar', __dirname + '/swagger-codegen-cli-2.3.1.jar']
+    '-jar', __dirname + '/swagger-codegen-cli-2.4.2.jar']
       .concat(process.argv.slice(2)), {stdio: 'inherit'})
