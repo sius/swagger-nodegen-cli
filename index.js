445ba92fbecb30f05b98a3adf1530328a0814fe6
Unknown <carmichael.john@aol.com>
2019-05-02 07:00:26 +0200

    chore: eslint added using javascript standard style and prepublish script to call linter

--- index.js
+++ index.js
@@ -1,16 +1,17 @@
+const path = require('path')
 module.exports = (args) => {
   return new Promise((resolve, reject) => {
     const spawn = require('child_process').spawn
     const java = spawn(
       'java',
-      ['-jar', __dirname + '/bin/swagger-codegen-cli-2.4.4.jar'].concat(args),
+      ['-jar', path.join(__dirname, '/bin/swagger-codegen-cli-2.4.4.jar')].concat(args),
       { stdio: 'inherit' }
     )
     java.on('close', (code) => {
-      if(code === 0){
+      if (code === 0) {
         return resolve(code)
       }
       reject(code)
-    });
+    })
   })
 }
