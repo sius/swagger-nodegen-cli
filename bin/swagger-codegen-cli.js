fa4e266eef77c58ec25a3fcaccd85ebf73de51ec
Brett McGillis <brett.mcgillis@mitel.com>
2019-05-16 13:09:55 -0400

    Updating swagger-codegen-cli jar with custom mitel version. Update swagger-codegen-cli.js to call new jar file. Update version, url and author in package.json

--- bin/swagger-codegen-cli.js
+++ bin/swagger-codegen-cli.js
@@ -1,5 +1,8 @@
-#!/usr/bin/env node
+//This version of the swagger-nodegen-cli launches a customized version of the swagger-codegen-cli-2.4.5.jar
+//This custom jar was built using the source for swagger-codegen-cli-2.4.5 with the addition of the following PR
+//https://github.com/swagger-api/swagger-codegen/pull/8845
+#!/usr/bin / env node
 require('child_process')
   .spawn('java', [
-    '-jar', __dirname + '/swagger-codegen-cli-2.4.4.jar']
+    '-jar', __dirname + '/swagger-codegen-cli-2.4.5-mitel.1.jar']
       .concat(process.argv.slice(2)), {stdio: 'inherit'})
